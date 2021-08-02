4556a64e323eb3e0fe3b251c4266a892ea26b8e5
chenghaoguo <chenghaoguo@mapxus.com>
2021-08-02 14:17:34 +0800

    [DMSVC-247] Developer can get visual map Add visual example file

--- example/src/VisualMap.tsx
+++ example/src/VisualMap.tsx
@@ -1,11 +1,45 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, TouchableOpacity, Image, StyleSheet} from 'react-native';
 import MapxusSdk from '@mapxus/react-native-mapxus-sdk';
 
 export default function VisualMap() {
+	const [active, setActive] = useState(false);
+
 	return (
 		<View style={{flex: 1}}>
-			<Text>visual map</Text>
+			<MapxusSdk.MapxusMap mapOption={{
+				buildingId: 'tsuenwanplaza_hk_369d01',
+				zoomInsets: {left: -100, right: -100}
+			}}>
+				<MapxusSdk.MapView style={{flex: 1}}/>
+			</MapxusSdk.MapxusMap>
+			<TouchableOpacity
+				style={styles.iconArea}
+				onPress={() => setActive(!active)}
+			>
+				{
+					active
+						? <Image
+							source={require('./assets/onIcon360.png')}
+							style={{width: '100%', height: '100%'}}
+						/>
+						: <Image
+							source={require('./assets/offIcon360.png')}
+							style={{width: '100%', height: '100%'}}
+						/>
+				}
+
+			</TouchableOpacity>
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	iconArea: {
+		position: 'absolute',
+		left: 10,
+		top: 50,
+		width: 40,
+		height: 40
+	}
+});
