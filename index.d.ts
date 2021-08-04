45081cccff17129ee5910fb3da7460d83153d635
chenghaoguo <chenghaoguo@mapxus.com>
2021-08-04 11:20:36 +0800

    [DMSVC-247] Developer can get visual map # Defining the response data format

--- index.d.ts
+++ index.d.ts
@@ -577,11 +577,11 @@ export interface VisualNodeViewProps extends ViewProps {
 }
 
 export interface VisualViewProps extends ViewProps {
-  onLoadFail?: () => void;
+  onLoadFail?: (error: VisualViewError) => void;
   onRenderComplete?: () => void;
-  onLoadingChanged?: (feature: object) => void;
-  onBearingChanged?: (feature: object) => void;
-  onNodeChanged?: (feature: object) => void;
+  onLoadingChanged?: (feature: VisualViewLoadingChangeObject) => void;
+  onBearingChanged?: (feature: BearingChangeObject) => void;
+  onNodeChanged?: (feature: NodeChangeObject) => void;
 }
 
 export interface AndroidLocationErronInfo {
@@ -934,6 +934,23 @@ export interface LightProps extends Omit<ViewProps, 'style'> {
 export interface VisualCoordinate2D {
   x: number;
   y: number;
+}
+
+export interface VisualViewError {
+  code: number;
+  message: string;
+}
+
+export interface VisualViewLoadingChangeObject {
+  isLoading: boolean;
+}
+
+export interface BearingChangeObject {
+  bearing: number;
+}
+
+export interface NodeChangeObject {
+  node: VisualNode;
 }
 
 export interface TappedOnPoiObject {
